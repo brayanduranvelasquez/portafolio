cd9f791a793ad2311288d7fee7f9e91fdf2d4e91
Brayan DVelasquez <brayan13s133@gmail.com>
2020-06-29 16:17:51 -0430

    Creado layout error
    Por ahora, al introducir una ruta no existente, redireccionara a la page Inicio

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="pantalla">
+    </div>
+</template>
+
+<script>
+    export default {
+        beforeCreate(){
+            this.$router.push('/')
+        }
+    }
+</script>
+
+<style lang="scss">
+    .pantalla {
+        top: 0;
+        z-index: 20000;
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        background: white;
+    }
+</style>
